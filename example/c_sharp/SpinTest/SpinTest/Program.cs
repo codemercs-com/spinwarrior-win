65d36f0aeda9f448bf6db5b8d12de9fccad5c834
codemercs-com <gitcodemercs.com>
2023-07-05 10:32:13 +0200

    Add C# demo

---
+++ example/c_sharp/SpinTest/SpinTest/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Simple demo to read data from the SpinWarrior family in C#.
+ */
+
+using System.Text;
+using static SpinKit.Functions;
+using static SpinKit.Defines;
+
+IntPtr m_Handle = SpinKit.Functions.SpinKitOpenDevice();
+SPINKIT_DATA m_Data = new SPINKIT_DATA();
+short[] serial = new short[9];
+uint pid = 0;
+uint encoder_count = 0;
+uint switch_count = 0;
+
+//Get first device and HANDLE found.
+if (m_Handle != IntPtr.Zero)
+{
+    if(SpinKitGetNumDevs() == 1)
+    {
+        Console.WriteLine("Single SpinWarrior Found");
+
+        SpinKitSetTimeout(m_Handle, 1000); //Timeout, if you use Read instead of ReadNonBlocking
+        pid = SpinKitGetProductId(m_Handle);
+
+        switch(pid)
+        {
+            case SPINKIT_PID24A3: //Obsolete device
+                Console.Write("SpinWarrior24 A3 found. SN: ");
+                encoder_count = 3;
+                switch_count = 6;
+                break;
+            case SPINKIT_PID24R4: //Obsolete device
+                Console.Write("SpinWarrior24 R4 found. SN: ");
+                encoder_count = 4;
+                switch_count = 7;
+                break;
+            case SPINKIT_PID24R6: //Obsolete device
+                Console.Write("SpinWarrior24 R6 found. SN: ");
+                encoder_count = 6;
+                switch_count = 3;
+                break;
+            case SPINKIT_PID28A3:
+                Console.Write("SpinWarrior28 A3 found. SN: ");
+                encoder_count = 3;
+                switch_count = 6;
+                break;
+            case SPINKIT_PID28R4:
+                Console.Write("SpinWarrior28 R4 found. SN: ");
+                encoder_count = 4;
+                switch_count = 7;
+                break;
+            case SPINKIT_PID28R6:
+                Console.Write("SpinWarrior28 R6 found. SN: ");
+                encoder_count = 6;
+                switch_count = 3;
+                break;
+        }
+
+        StringBuilder sn = new StringBuilder();
+        SpinKitGetSerialNumber(m_Handle, sn);
+        Console.WriteLine(sn);
+
+        //Read 100x the SpinWarrior
+        for (int i = 0; i < 100; i++)
+        {
+            //Read data non blocking from SpinWarrior
+            SpinKitReadNonBlocking(m_Handle, ref m_Data); 
+
+            Console.Write("Spin Data: ");
+
+            //Get encoder
+            for (int j = 0; j < encoder_count; j++)
+            {
+                Console.Write(m_Data.encoder[j]);
+                Console.Write(" | ");
+            }
+
+            //Get buttons as bool array
+            for (int j = 0; j < switch_count; j++)
+            {
+                if (m_Data.buttons[j] == true)
+                    Console.Write("1");
+                else
+                    Console.Write("0");
+            }
+
+            Console.WriteLine("");
+            Thread.Sleep(100);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Multiple SpinWarrior Found. For this demo please connect only one device");
+    }
+}
+else
+{
+    Console.WriteLine("No SpinWarrior found");
+}
+
+//Don't forget to close the spinkit.dll
+SpinKit.Functions.SpinKitCloseDevice(m_Handle);
---
+++ example/c_sharp/SpinTest/SpinTest/spinkit.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SpinKit
+{
+    public static class Defines
+    {
+        // IO-Warrior vendor & product IDs
+        public const uint SPINKIT_VENDOR_ID = 0x07c0;
+        public const uint SPINKIT_VID = SPINKIT_VENDOR_ID;
+
+        // SpinWarrior24R4
+        public const uint SPINKIT_PRODUCT_ID24R4 = 0x1200;
+        public const uint SPINKIT_PID24R4 = SPINKIT_PRODUCT_ID24R4;
+
+        // SpinWarrior24R6
+        public const uint SPINKIT_PRODUCT_ID24R6 = 0x1201;
+        public const uint SPINKIT_PID24R6 = SPINKIT_PRODUCT_ID24R6;
+
+        // SpinWarrior24A3
+        public const uint SPINKIT_PRODUCT_ID24A3 = 0x1202;
+        public const uint SPINKIT_PID24A3 = SPINKIT_PRODUCT_ID24A3;
+
+        // SpinWarrior28R4
+        public const uint SPINKIT_PRODUCT_ID28R4 = 0x1209;
+        public const uint SPINKIT_PID28R4 = SPINKIT_PRODUCT_ID28R4;
+
+        // SpinWarrior28R6
+        public const uint SPINKIT_PRODUCT_ID28R6 = 0x120A;
+        public const uint SPINKIT_PID28R6 = SPINKIT_PRODUCT_ID28R6;
+
+        // SpinWarrior28A3
+        public const uint SPINKIT_PRODUCT_ID28A3 = 0x1208;
+        public const uint SPINKIT_PID28A3 = SPINKIT_PRODUCT_ID28A3;
+		
+		// iTrain
+        public const uint SPINKIT_PRODUCT_MAZERO = 0x1281;
+        public const uint SPINKIT_PIDIMAZERO = SPINKIT_PRODUCT_MAZERO;
+
+        // Max number of SpinWarriors supported
+        public const uint SPINKIT_MAX_DEVICES = 16;
+		
+		//Length of serial number
+		public const uint SPINKIT_SN_LEN = 8;
+    }
+
+    public static class Functions
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SPINKIT_DATA
+        {
+            public IntPtr spinHandle;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 6, ArraySubType = UnmanagedType.I4)] public Int32[] encoder;
+            [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 7, ArraySubType = UnmanagedType.I4)] public Boolean[] buttons;
+        }
+
+        //Import functions from iowkit.dll (win32)
+        [DllImport("spinkit.dll")] public static extern IntPtr SpinKitOpenDevice();
+        [DllImport("spinkit.dll")] public static extern void SpinKitCloseDevice(IntPtr spinHandle);
+        [DllImport("spinkit.dll")] public static extern bool SpinKitRead(IntPtr spinHandle, byte[] buffer);
+        [DllImport("spinkit.dll")] public static extern bool SpinKitReadNonBlocking(IntPtr spinHandle, ref SPINKIT_DATA data);
+        [DllImport("spinkit.dll")] public static extern UInt32 SpinKitGetNumDevs();
+        [DllImport("spinkit.dll")] public static extern IntPtr SpinKitGetDeviceHandle(UInt32 numDevice);
+        [DllImport("spinkit.dll")] public static extern UInt32 SpinKitGetProductId(IntPtr spinHandle);
+        [DllImport("spinkit.dll")] public static extern UInt32 SpinKitGetRevision(IntPtr spinHandle);
+        [DllImport("spinkit.dll", CharSet = CharSet.Unicode)] public static extern bool SpinKitGetSerialNumber(IntPtr spinHandle, StringBuilder SnTarget);
+        [DllImport("spinkit.dll")] public static extern bool SpinKitSetTimeout(IntPtr spinHandle, UInt32 timeout);
+        [DllImport("spinkit.dll")] public static extern string SpinKitVersion();
+
+        //Import GetLastError (win32)
+        [DllImport("kernel32.dll")] public static extern uint GetLastError();
+    }
+}
